4a1459779804279158ffb13f8fd8003996442eac
Lrubick <liguofeng1127@gmail.com>
2021-02-24 13:10:13 +0800

    项目修改

--- MedSPA/assets/Script/HelloWorld.ts
+++ MedSPA/assets/Script/HelloWorld.ts
@@ -1,19 +1,35 @@
-const { ccclass, property } = cc._decorator; // 从 cc._decorator 命名空间中引入 ccclass 和 property 两个装饰器
+const { ccclass, property } = cc._decorator;
 
-@ccclass // 使用装饰器声明 CCClass
-export default class HelloWorld extends cc.Component { // ES6 Class 声明语法，继承 cc.Component
+@ccclass
+export default class Helloworld extends cc.Component {
 
-    @property(cc.Label)     // 使用 property 装饰器声明属性，括号里是属性类型，装饰器里的类型声明主要用于编辑器展示
-    label: cc.Label = null; // 这里是 TypeScript 用来声明变量类型的写法，冒号后面是属性类型，等号后面是默认值
+    @property(cc.Label)
+    label: cc.Label = null;
 
-    // 也可以使用完整属性定义格式
-    @property({
-        visible: false
-    })
+    @property
     text: string = 'hello';
 
-    // 成员方法
-    onLoad() {
+    @property(cc.Node)
+    planet: cc.Node = null;
+
+    @property(cc.ProgressBar)
+    progressBar: cc.ProgressBar = null;
+
+    start() {
         // init logic
+        // this.label.string = this.text;
+        cc.tween(this.planet)
+            .by(2, { angle: 360 })
+            .repeatForever()
+            .start()
+
+        const self = this;
+        cc.director.preloadScene("main", function (completedCount: number, totalCount: number, item: Object) {
+            self.label.string = completedCount / totalCount * 100 + "%";
+            self.progressBar.progress = completedCount / totalCount;
+        }, () => {
+            cc.log("Next scene preloaded");
+        });
+
     }
 }
